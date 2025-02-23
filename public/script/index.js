7958310361237822d8582bd8b30285a1c0e0afa4
unknown <jackhasakeyboard@gmailcom>
2025-02-23 09:09:10 -0700

    Added library for shaders, output solid shader as a default. Functionality for OpenGL shader classes replaced

--- public/script/index.js
+++ public/script/index.js
@@ -1,8 +1,11 @@
+const wd = 800;
+const ht = 600;
+
 document.addEventListener("DOMContentLoaded", async function() {
 	window.canv = document.getElementById("disp");
 	
 	window.ctx = canv.getContext("2d");
 
 	ctx.fillStyle = bg;
-	ctx.fillRect(0, 0, 800, 600);
+	ctx.fillRect(0, 0, wd, ht);
 });
--- public/script/shad.js
+++ public/script/shad.js
@@ -1,63 +1,7 @@
-class Shad {
-	id;
-
-	constructor(name, stage) {
-		let ext;
-		let type;
-		switch (stage) {
-			case 0:
-				type = gl.VERTEX_SHADER;
-				ext = "vs";
-
-				break;
-
-			case 1:
-				type = gl.FRAGMENT_SHADER;
-				ext = "fs";
-
-				break;
-		}
-
-		const buff = Fs.rd(Fs.resPath + "/shad/" + name + "." + ext);
-
-		this.id = gl.createShader(type);
-		gl.shaderSource(this.id, buff);
-
-		gl.compileShader(this.id);
-		if (!gl.getShaderParameter(this.id, gl.COMPILE_STATUS)) {
-			console.error("Error compiling shader", gl.getShaderInfoLog(this.id));
-		}
-	}
+function solid(x, y) {
+	return true;
 }
 
-class Prog {
-	id;
-
-	constructor(nameVtx, nameFrag) {
-		this.id = gl.createProgram();
-
-		let shadVtx = new Shad(nameVtx, 0);
-		let shadFrag = new Shad(nameFrag, 1);
-
-		gl.attachShader(this.id, shadVtx.id);
-		gl.attachShader(this.id, shadFrag.id);
-
-		gl.linkProgram(this.id);
-		if (!gl.getProgramParameter(this.id, gl.LINK_STATUS)) {
-			console.error("Error linking program", gl.getProgramInfoLog(this.id));
-		}
-
-		gl.validateProgram(this.id);
-		if (!gl.getProgramParameter(this.id, gl.VALIDATE_STATUS)) {
-			console.error("Error validating program", gl.getProgramInfoLog(this.id));
-		}
-	}
-
-	use() {
-		gl.useProgram(this.id);
-	}
-
-	unUse() {
-		gl.useProgram(null);
-	}
+function checker(x, y, ln) {
+	blitPix(x, y);
 }
