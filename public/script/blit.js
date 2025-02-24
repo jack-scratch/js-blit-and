5ec6c859777f2b644523bf2289560f51f48d87db
unknown <jackhasakeyboard@gmailcom>
2025-02-23 19:39:34 -0700

    Removed setting of "active" color in every call to blit

--- public/script/blit.js
+++ public/script/blit.js
@@ -1,17 +1,11 @@
 function blitRect(x, y, wd, ht) {
-	ctx.fillStyle = js;
-
 	ctx.fillRect(x, y, wd, ht);
 }
 
 function blitSq(x, y, ln) {
-	ctx.fillStyle = js;
-
 	blitRect(x, y, ln, ln);
 }
 
 function blitPix(x, y) {
-	ctx.fillStyle = js;
-
 	blitSq(x, ht - y, 1);
 }
