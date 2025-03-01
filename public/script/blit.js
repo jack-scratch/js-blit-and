492eefb569225be76a776249a4beaa2bafdac5ab
unknown <jackhasakeyboard@gmailcom>
2025-03-01 02:53:34 -0700

    Created utility to draw line, not quantized

--- public/script/blit.js
+++ public/script/blit.js
@@ -21,3 +21,18 @@ function blitVLine(x, y, ln) {
 		blitPix(x, y + i);
 	}
 }
+
+function blitLine(startX, startY, endX, endY) {
+	const slope = (endY - startY) / (endX - startX);
+
+	let dir = 1;
+	let baseY = startY;
+	if (endX < startX) {
+		dir = -1;
+		baseY = endY;
+	}
+
+	for (let x = 0; x < Math.abs(endX - startX); x++) {
+		blitPix(startX + (x * dir), baseY + (x * slope));
+	}
+}
