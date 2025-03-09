ff482c2cb4e3601195e42c23fd3712f8cbfbd73d
unknown <jackhasakeyboard@gmailcom>
2025-03-09 10:06:36 -0600

    Updated canvas dynamically based on variables

--- public/script/index.js
+++ public/script/index.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", async function() {
 	window.canv = document.getElementById("disp");
+
+	canv.width = wd;
+	canv.height = ht;
 	
 	window.ctx = canv.getContext("2d");
 
