ceb051010c404daa4ec998a2ab05c3226f539271
Jack Alma <jackhasakeyboard@gmail.com>
2026-04-30 11:37:30 -0600

    "blit" "and" operation on multiple ^fmt"plural["object"]"

--- public/script/index.js
+++ public/script/index.js
@@ -11,6 +11,25 @@ document.addEventListener("DOMContentLoaded", async function() {
 
 	ctx.fillStyle = js;
 
+	const ln = 100;
+	const margin = 8 * 2;
+	const bOffset = 16;
+	for (let y = 0; y < ht; y++) {
+		for (let x = 0; x < wd; x++) {
+			if (
+				(
+					x > margin && x < margin + ln &&
+					y > margin && y < margin + ln
+				) & (
+					x > margin + bOffset && x < margin + bOffset + ln &&
+					y > margin + bOffset && y < margin + bOffset + ln
+				)
+			) {
+				ctx.fillRect(x, y, 1, 1);
+			}
+		}
+	}
+
 	const btn = document.getElementById("save");
 	const link = document.createElement("a");
 	link.download = "blit.png";
